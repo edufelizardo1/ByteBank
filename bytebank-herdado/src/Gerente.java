daba1bbab624cb9b777553a726be82fe4a90bc5a
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-22 18:42:06 -0300

    Update: Gerente

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,21 +1,5 @@
 
-public class Gerente extends Funcionario{
-
-	private int senha;
-	
-	
-	public boolean autenticacao(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
+public class Gerente extends FuncionarioAutenticavel{
 
 
 	public double getBonificacao() {
