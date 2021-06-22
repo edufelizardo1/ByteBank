f25295f8949ef8a865659aa2fa77127cece9fb7e
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-22 19:17:31 -0300

    Update: Cliente.java

--- bytebank-herdado/src/Cliente.java
+++ bytebank-herdado/src/Cliente.java
@@ -1,4 +1,21 @@
 
-public class Cliente extends Autenticavel {
+public class Cliente implements Autenticavel {
+
+	private int senha;
+
+	@Override
+	public boolean autenticacao(int senha) {
+
+		if (this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
 
 }
