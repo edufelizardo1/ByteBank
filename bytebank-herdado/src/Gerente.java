b24aaa634ed3429bdb4caadf88ba6290d128dee4
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-22 19:17:48 -0300

    Upadate: Gerente

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,9 +1,21 @@
 
-public class Gerente extends Funcionario{
+public class Gerente extends Funcionario implements Autenticavel{
 
 
 	public double getBonificacao() {
 		return (super.getSalario() * 0.5) + super.getSalario();
+	}
+
+	@Override
+	public boolean autenticacao(int senha) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
