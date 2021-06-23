5010d961bf96f2592d81dc5bb08fb44166153e90
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-23 19:35:30 -0300

    Update Fluxo.java

--- java-pilha/src/fluxo/Fluxo.java
+++ java-pilha/src/fluxo/Fluxo.java
@@ -4,13 +4,23 @@ public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        try{
+            metodo1();
+        } catch(ArithmeticException ex) {
+            String msg = ex.getMessage();
+            System.out.println("ArithmeticException " + msg);
+            ex.printStackTrace();
+        }
         System.out.println("Fim do main");
     }
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
-        metodo2();
+        try{
+            metodo2();
+        } catch(ArithmeticException ex) {
+            System.out.println("ArithmeticException");
+        }
         System.out.println("Fim do metodo1");
     }
 
@@ -18,6 +28,11 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+            try{
+                int a = i / 0;
+            } catch(ArithmeticException ex) {
+                System.out.println("ArithmeticException");
+            }
         }
         System.out.println("Fim do metodo2");
     }
