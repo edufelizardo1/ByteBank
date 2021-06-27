74d29da8d53f88c1d8819a24820b04f81c6d13c4
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-27 10:05:03 -0300

    Update Designer.java

--- bytebank-herdado/src/Designer.java
+++ bytebank-herdado/src/Designer.java
@@ -1,25 +1,9 @@
-
-public class Designer extends Funcionario{
-
-	private int senha;
+//Gerente eh um Funcionario, Gerente herda da class Funcionario
+public class Designer extends Funcionario {
 	
-	
-	public boolean autenticacao(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
-
-
 	public double getBonificacao() {
-		return super.getSalario() + 200.0;
-	}
-	
+		System.out.println("Chamando o método de bonificacao do Designer");
+		return 200;
+	}	
+
 }
