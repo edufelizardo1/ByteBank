ba1faaf36d37584f2e449d7c4a8976daf64ae0a3
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-27 10:05:38 -0300

    Update TesteGerente.java

--- bytebank-herdado/src/TesteGerente.java
+++ bytebank-herdado/src/TesteGerente.java
@@ -1,9 +1,13 @@
 
 public class TesteGerente {
+
 	public static void main(String[] args) {
+		
+		Autenticavel referencia = new Cliente();
+		
 		Gerente g1 = new Gerente();
-		g1.setNome("Marcos");
-		g1.setCpf("333.333.333-33");
+		g1.setNome("Marco");
+		g1.setCpf("235568413");
 		g1.setSalario(5000.0);
 		
 		System.out.println(g1.getNome());
@@ -11,9 +15,12 @@ public class TesteGerente {
 		System.out.println(g1.getSalario());
 		
 		g1.setSenha(2222);
-		boolean autenticou = g1.autenticacao(2222);
+		boolean autenticou = g1.autentica(2222);
 		
 		System.out.println(autenticou);
+		
 		System.out.println(g1.getBonificacao());
+
 	}
+
 }
