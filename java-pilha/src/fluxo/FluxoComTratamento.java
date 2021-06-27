10871e553e38f77fcf9201697d2fc17125b2a01d
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-26 22:05:02 -0300

    Update FluxoComTratamento.java

--- java-pilha/src/fluxo/FluxoComTratamento.java
+++ java-pilha/src/fluxo/FluxoComTratamento.java
@@ -6,28 +6,25 @@ public class FluxoComTratamento {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        try{
+        try {
             metodo1();
-        } catch(ArithmeticException ex) {
+        } catch(ArithmeticException | NullPointerException | MinhaExcecao ex) {
             String msg = ex.getMessage();
-            System.out.println("ArithmeticException " + msg);
+            System.out.println("Exception " + msg);
             ex.printStackTrace();
-        }
+        } 
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() {
+    private static void metodo1() throws MinhaExcecao {
         System.out.println("Ini do metodo1");
-        try{
-            metodo2();
-        } catch(ArithmeticException ex) {
-            System.out.println("ArithmeticException");
-        }
+        metodo2();
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() {
+    private static void metodo2() throws MinhaExcecao{
         System.out.println("Ini do metodo2");
-        throw new ArithmeticException("Deu erro");
+        throw new MinhaExcecao("deu muito errado");
+        //System.out.println("Fim do metodo2");        
     }
 }
