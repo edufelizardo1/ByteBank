61cc4ec7274303734ddbfb4c9c49a12b0b6916dc
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-27 10:04:34 -0300

    Update Administrador.java

--- bytebank-herdado/src/Administrador.java
+++ bytebank-herdado/src/Administrador.java
@@ -1,25 +1,25 @@
 
-public class Administrador extends Funcionario implements Autenticavel {
+public class Administrador extends Funcionario implements Autenticavel{
 
+	private AutenticacaoUtil autenticador;
+
+	public Administrador() {
+		this.autenticador = new AutenticacaoUtil();
+	}
+	
 	@Override
 	public double getBonificacao() {
 		return 50;
 	}
-
-	private AutenticaUtil autenticador;
-
-	public Administrador() {
-		this.autenticador  = new AutenticaUtil();
-	}
-
+	
 	@Override
 	public void setSenha(int senha) {
 		this.autenticador.setSenha(senha);
 	}
-	
+
 	@Override
-	public boolean autenticacao(int senha) {
-		return this.autenticador.autenticacao(senha);
-		
+	public boolean autentica(int senha) {
+		return this.autenticador.autentica(senha);
 	}
+
 }
