8f32bfdf05bf8322f1e55a9fbfdced329962a407
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-27 10:05:24 -0300

    Update Gerente.java

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,27 +1,25 @@
-
-public class Gerente extends Funcionario implements Autenticavel {
-
-	private int senha;
-
-	public double getBonificacao() {
-		return (super.getSalario() * 0.5) + super.getSalario();
-	}
-
-	private AutenticaUtil autenticador;
+//Gerente eh um Funcionario, Gerente herda da class Funcionario, assina o contrato Autenticavel, eh um Autenticavel
+public class Gerente extends Funcionario implements Autenticavel{
+	
+	private AutenticacaoUtil autenticador;
 
 	public Gerente() {
-		this.autenticador  = new AutenticaUtil();
+		this.autenticador = new AutenticacaoUtil();
+	}
+	
+	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificacao do GERENTE");
+		return super.getSalario();
 	}
 
 	@Override
 	public void setSenha(int senha) {
 		this.autenticador.setSenha(senha);
 	}
-	
+
 	@Override
-	public boolean autenticacao(int senha) {
-		return this.autenticador.autenticacao(senha);
-		
+	public boolean autentica(int senha) {
+		return this.autenticador.autentica(senha);
 	}
 
 }
