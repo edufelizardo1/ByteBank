de3d43bf4a91acb753afb9695a24458bfeda0325
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-27 10:05:09 -0300

    Update EditorVideo.java

--- bytebank-herdado/src/EditorVideo.java
+++ bytebank-herdado/src/EditorVideo.java
@@ -1,25 +1,9 @@
-
-public class EditorVideo extends Funcionario{
-
-	private int senha;
+//Gerente eh um Funcionario, Gerente herda da class Funcionario
+public class EditorVideo extends Funcionario {
 	
-	
-	public boolean autenticacao(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
-
-
 	public double getBonificacao() {
-		return super.getSalario() + 150.0;
-	}
-	
+		System.out.println("Chamando o método de bonificacao do Editor de video");
+		return 150;
+	}	
+
 }
