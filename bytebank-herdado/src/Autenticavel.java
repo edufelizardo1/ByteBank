5fdf61d36f95884afcc12ef619c6f01cd63dd541
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-27 10:04:39 -0300

    Update Autenticavel.java

--- bytebank-herdado/src/Autenticavel.java
+++ bytebank-herdado/src/Autenticavel.java
@@ -1,14 +1,11 @@
 
-// contrato autenticavel
-//quem assin esse contrato
-//precisa implementar
-//metodo setSenha
-//metodo autentica
-
+//contrato Autenticavel
+	//quem assina esse contrato, precisa implementar
+		//metodo setSenha
+		//metodo autentica
 public abstract interface Autenticavel {
-
-	public abstract boolean autenticacao(int senha);
-
-	public abstract void setSenha(int senha);
 	
+	public abstract void setSenha(int senha) ;
+	
+	public abstract boolean autentica(int senha) ;
 }
