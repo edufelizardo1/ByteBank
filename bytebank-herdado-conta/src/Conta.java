b95a09ca6fb5e2f8cc80ce6069bd0a33096dfc9a
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-27 10:03:29 -0300

    Update Conta.java

--- bytebank-herdado-conta/src/Conta.java
+++ bytebank-herdado-conta/src/Conta.java
@@ -1,4 +1,5 @@
 
+
 public abstract class Conta {
 
     protected double saldo;
@@ -6,34 +7,30 @@ public abstract class Conta {
     private int numero;
     private Cliente titular;
     private static int total = 0;
-
+    
     public Conta(int agencia, int numero){
         Conta.total++;
-        System.out.println("O total de contas é " + Conta.total);
+        //System.out.println("O total de contas Ã© " + Conta.total);
         this.agencia = agencia;
         this.numero = numero;
-        this.saldo = 100;
-        System.out.println("Estou criando uma conta " + this.numero);
+        //this.saldo = 100;
+        //System.out.println("Estou criando uma conta " + this.numero);
     }
 
     public abstract void deposita(double valor);
 
-    public boolean saca(double valor) {
-        if(this.saldo >= valor) {
-            this.saldo -= valor;
-            return true;
-        } else {
-            return false;
-        }
+    public void saca(double valor) throws SaldoInsuficienteException{
+    	
+        if(this.saldo < valor) {
+            throw new SaldoInsuficienteException("Saldo: " + this.saldo + ", Valor: " + valor);
+        } 
+        
+        this.saldo -= valor;       
     }
 
-    public boolean transfere(double valor, Conta destino) {
-        if(this.saca(valor)) {
-                destino.deposita(valor);
-                return true;
-        } else {
-                return false;
-        }
+    public void transfere(double valor, Conta destino) throws SaldoInsuficienteException{
+        this.saca(valor);
+        destino.deposita(valor);
     }
 
     public double getSaldo(){
