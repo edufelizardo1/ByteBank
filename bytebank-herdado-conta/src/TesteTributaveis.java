538e4267105449a0ba6a4b19f5a0643ec2f5e054
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-27 10:04:23 -0300

    Update TesteTributaveis.java

--- bytebank-herdado-conta/src/TesteTributaveis.java
+++ bytebank-herdado-conta/src/TesteTributaveis.java
@@ -2,15 +2,17 @@
 public class TesteTributaveis {
 
 	public static void main(String[] args) {
-		ContaCorrente cc = new ContaCorrente(222, 333);
-		cc.deposita(100);
+		ContaCorrente cc= new ContaCorrente(222, 333);
+		cc.deposita(100.0);
 		
 		SeguroDeVida seguro = new SeguroDeVida();
 		
-		CalculadorImposto calc = new CalculadorImposto();
+		CalculadorDeImposto calc = new CalculadorDeImposto();
 		calc.registra(cc);
 		calc.registra(seguro);
 		
 		System.out.println(calc.getTotalImposto());
+
 	}
+
 }
