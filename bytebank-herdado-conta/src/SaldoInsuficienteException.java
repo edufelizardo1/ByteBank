f1f1301009ed961311c0f84d33a89539e4607e8b
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-28 07:26:53 -0300

    Update SaldoInsuficienteException.java

--- bytebank-herdado-conta/src/SaldoInsuficienteException.java
+++ bytebank-herdado-conta/src/SaldoInsuficienteException.java
@@ -1,6 +1,7 @@
 
 public class SaldoInsuficienteException extends Exception{
-	
+	private static final long serialVersionUID = 1L;
+
 	public SaldoInsuficienteException(String msg) {
 		super(msg);
 	}
