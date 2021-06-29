dcd0f8c2747405d2af395c52142d554874e9bfa4
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-29 08:03:55 -0300

    Update Cliente.java

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Cliente.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Cliente.java
@@ -1,12 +1,13 @@
 package br.com.bytebank.banco.modelo;
 
+
 /**
- * Classe que representa um cliente no Bytebank 
- * 
+ * Classe que representa um cliente no Bytebank.
+ *
  * @author Eduardo Felizardo
  * @version 0.1
  */
-public class Cliente {
+public class Cliente  {
 
     private String nome;
     private String cpf;
