2f3d321e6f4950d6b85535127863fbb8e4a57b18
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-29 08:04:44 -0300

    Create Teste.java

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
@@ -0,0 +1,16 @@
+package br.com.bytebank.banco.teste;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+
+		int[] idade1 = new int[] {29, 39, 19, 69, 59};
+		
+		for (int i : idade1) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+	}
+
+}
