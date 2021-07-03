aaf38d8d7eaff3ebf9469124611e295a46059e18
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-07-03 19:37:18 -0300

    Create TesteEscrita3.java

---
+++ java-io/src/com/java/io/teste/TesteEscrita3.java
@@ -0,0 +1,26 @@
+package com.java.io.teste;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class TesteEscrita3 {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		// Fluxo de entrada de arquivo
+		
+//		OutputStream fos = new FileOutputStream("lorem2.txt");
+//		Writer osw = new OutputStreamWriter(fos);
+//		BufferedWriter bw = new BufferedWriter(osw);
+		
+		PrintStream ps = new PrintStream("lorem2.txt");
+		
+		ps.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ");
+		ps.println();
+		ps.println("ihjorewhroheo iorewjojw kkew");
+		
+		ps.close();
+	}
+
+}
