d84f9c241b9d421f5adfccfb4fc65a2008e1d4a3
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-07-03 20:31:03 -0300

    Update TesteLeitura2.java

--- java-io/src/com/java/io/teste/TesteLeitura2.java
+++ java-io/src/com/java/io/teste/TesteLeitura2.java
@@ -1,6 +1,8 @@
 package com.java.io.teste;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeitura2 {
@@ -13,8 +15,22 @@ public class TesteLeitura2 {
 		while (scanner.hasNextLine()) {
 			
 			String linha = scanner.nextLine();
-			
 			System.out.println(linha);
+			
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useLocale(Locale.US);
+			linhaScanner.useDelimiter(",");
+			
+			String valor1 = linhaScanner.next();
+			int valor2 = linhaScanner.nextInt();
+			int valor3 = linhaScanner.nextInt();
+			String valor4 = linhaScanner.next();
+			double valor5 = linhaScanner.nextDouble();
+			
+			System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
+			
+//			String[] valores = linha.split(",");
+//			System.out.println(Arrays.toString(valores));
 		}
 		
 		scanner.close();
