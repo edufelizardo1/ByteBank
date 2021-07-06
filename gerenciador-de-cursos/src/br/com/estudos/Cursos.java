06a776d067682db9b10da30685c918347d90488d
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-07-06 17:20:06 -0300

    Create Cursos.java

---
+++ gerenciador-de-cursos/src/br/com/estudos/Cursos.java
@@ -0,0 +1,36 @@
+package br.com.estudos;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Cursos {
+
+	private String nome;
+	private String instrutor;
+	private List<Aula> aulas = new LinkedList<Aula>();
+	
+	
+	
+	public Cursos(String nome, String instrutor) {
+		super();
+		this.nome = nome;
+		this.instrutor = instrutor;
+	}
+
+	public List<Aula> getAulas() {
+		return Collections.unmodifiableList(aulas);
+	}
+	
+	public String getInstrutor() {
+		return instrutor;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void adiciona(Aula aula) {
+		this.aulas.add(aula);
+	}
+}
