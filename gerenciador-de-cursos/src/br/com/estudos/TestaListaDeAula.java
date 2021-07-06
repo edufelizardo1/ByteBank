adfc89697150c089bbfc317db47a2f5216f6f183
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-07-06 14:23:54 -0300

    Create TestaListaDeAula.java

---
+++ gerenciador-de-cursos/src/br/com/estudos/TestaListaDeAula.java
@@ -0,0 +1,30 @@
+package br.com.estudos;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class TestaListaDeAula {
+
+	public static void main(String[] args) {
+		
+		Aula a1 = new Aula("Revisando ArrayLists", 21);
+		Aula a2 = new Aula("Lista de referências", 20);
+		Aula a3 = new Aula("relacionamento de lista e objetos", 15);
+		
+		ArrayList<Aula> aulas = new ArrayList<Aula>();
+		aulas.add(a1);
+		aulas.add(a2);
+		aulas.add(a3);
+		
+		System.out.println(aulas);
+		
+		Collections.sort(aulas);
+		
+		System.out.println(aulas);
+		
+		Collections.sort(aulas, Comparator.comparing(Aula::getTempo));
+		
+		System.out.println(aulas);
+	}
+}
