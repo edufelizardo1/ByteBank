e02274da3a30e29c2ffee759ad86944ee197881a
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-07-06 18:02:41 -0300

    Update Cursos.java

--- gerenciador-de-cursos/src/br/com/estudos/Cursos.java
+++ gerenciador-de-cursos/src/br/com/estudos/Cursos.java
@@ -33,4 +33,13 @@ public class Cursos {
 	public void adiciona(Aula aula) {
 		this.aulas.add(aula);
 	}
+	
+	public int getTempoTotal() {
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+	
+	@Override
+	public String toString() {
+		return "[Curso = " + nome + ", tempo total = " + this.getTempoTotal() + ", " +  aulas + "]";
+	}
 }
