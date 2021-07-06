83764c7ef4ae5dbdaf25228cd00a7a099f97ce08
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-07-06 19:03:53 -0300

    Update Aluno.java

--- gerenciador-de-cursos/src/br/com/estudos/Aluno.java
+++ gerenciador-de-cursos/src/br/com/estudos/Aluno.java
@@ -2,4 +2,25 @@ package br.com.estudos;
 
 public class Aluno {
 
+	private String nome;
+	private int numeromatricula;
+	
+	
+	public Aluno(String nome, int numeromatricula) {
+		super();
+		this.nome = nome;
+		this.numeromatricula = numeromatricula;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public int getNumeromatricula() {
+		return numeromatricula;
+	}
+	
+	
 }
